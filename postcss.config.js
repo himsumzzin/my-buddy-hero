552b93aa1b2ac7532b915f4baa43109d2e76ca9d
Ellis <nardi0302@gmail.com>
2022-12-06 14:59:53 +0900

    config: Setup postCSS(not finished)
    - postCSS 환경을 구성하였습니다.
    - NEXT 환경에서는 정상 동작하지만 storybook에서는 에러가 발생합니다. 추후 수정하겠습니다.

---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 2,
+        features: {
+          'custom-properties': false,
+          'nesting-rules': true,
+        },
+      },
+    ],
+    'postcss-normalize',
+  ],
+};
