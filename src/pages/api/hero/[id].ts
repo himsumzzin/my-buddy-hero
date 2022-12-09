801460ac95a43b6d5d65e1661657073ffa50daa5
who0803 <10rlatkdtn@naver.com>
2022-12-09 21:51:57 +0900

    feat: Create hero API
    - Hero 스키마 생성
    - hero 등록 api 구현
    - hero 삭제 api 구현
    - hero 전체 hero 받기 api 구현
    - hello.ts 삭제

---
+++ src/pages/api/hero/[id].ts
@@ -0,0 +1,67 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../../utils/dbConnect';
+import Hero from '../../../models/Hero';
+
+type Data = {
+  statusCode: number;
+  body: {
+    success: boolean;
+    data?: unknown[];
+  };
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { method } = req;
+  await dbConnect();
+
+  if (method === 'GET') {
+    Hero.find({ groupId: req.query.id })
+      .exec()
+      .then((results) => {
+        const data = results.map((result) => ({
+          heroId: result._id,
+          name: result.name,
+          title: result.title,
+          completeNumber: result.completeNumber,
+        }));
+
+        return res.status(200).json({
+          statusCode: 200,
+          body: {
+            success: true,
+            data,
+          },
+        });
+      })
+      .catch(() => {
+        return res.status(500).json({
+          statusCode: 500,
+          body: {
+            success: false,
+          },
+        });
+      });
+  } else if (method === 'DELETE') {
+    Hero.findOneAndDelete({ _id: req.query.id })
+      .exec()
+      .then(() => {
+        return res.status(200).json({
+          statusCode: 200,
+          body: {
+            success: true,
+          },
+        });
+      })
+      .catch(() => {
+        return res.status(500).json({
+          statusCode: 500,
+          body: {
+            success: false,
+          },
+        });
+      });
+  }
+}
---
+++ src/pages/api/hero/index.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../../utils/dbConnect';
+import Hero from '../../../models/Hero';
+
+type Data = {
+  statusCode: number;
+  body: {
+    success: boolean;
+  };
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { method } = req;
+  if (method !== 'POST') return;
+
+  await dbConnect();
+  const hero = new Hero({ ...req.body, completeNumber: 0 });
+  hero
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        statusCode: 201,
+        body: {
+          success: true,
+        },
+      });
+    })
+    .catch(() => {
+      return res.status(500).json({
+        statusCode: 500,
+        body: {
+          success: false,
+        },
+      });
+    });
+}
