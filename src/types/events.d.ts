d89d48bee07815b9fb4d3611f3dad8ccaf0e535d
Ellis <nardi0302@gmail.com>
2022-12-09 10:41:41 +0900

    feat: Extends Element interface
    - keyup / keydown 네이티브 이벤트 처리를 위한 타입을 확장하였습니다

---
+++ src/types/events.d.ts
@@ -0,0 +1,16 @@
+// keyup / keydown 이벤트에 대한 인터페이스 확장
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const focusableElements: Element[] = [];
+
+interface Element {
+  removeEventListener(
+    type: 'keyup' | 'keydown',
+    listener: (event: KeyboardEvent) => void,
+    options?: boolean | EventListenerOptions
+  ): void;
+  addEventListener(
+    type: 'keyup' | 'keydown',
+    listener: (event: KeyboardEvent) => void,
+    options?: boolean | EventListenerOptions
+  ): void;
+}
