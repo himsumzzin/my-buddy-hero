1f5a653acf28c1156fada675e8976c2c16f03a13
CHAERYEON-LEE <sogogijjang@naver.com>
2022-12-16 00:57:25 +0900

    feat: Create Register Page
    히어로 등록 페이지 구현
    - 세개의 컴포넌트를 동적으로 렌더링하는 페이지 구현
    - 세개의 컴포넌트에서 공유되는 heroInfo state를 가지고있음

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,47 +1,5 @@
-import { Input, Button } from '@/components/common';
-import styles from './Register.module.css';
+import { Register } from '@/components/pages/Register';
 
-export default function Register() {
-  return (
-    <>
-      <div className={styles.container}>
-        <h1 className={styles.title}>히어로 등록</h1>
-        <form action="" className={styles.form}>
-          <Input
-            name="herotitle"
-            size="lg"
-            labelText="히어로 칭호를 입력해주세요"
-            validText="히어로 칭호를 입력해주세요"
-            initialValue=""
-            className={styles.input}
-          ></Input>
-          <Input
-            name="herodescription"
-            size="lg"
-            labelText="히어로에 대해서 설명해주세요!"
-            validText="히어로에 대한 설명을 입력해주세요"
-            initialValue=""
-            className={styles.input}
-          ></Input>
-          <Input
-            name="heroname"
-            size="lg"
-            labelText="이름을 입력해주세요"
-            validText="이름을 입력하세요"
-            initialValue=""
-            className={styles.input}
-          ></Input>
-          <Input
-            name="herocode"
-            size="lg"
-            labelText="히어로 넘버를 입력하세요. 쉿! 비밀~"
-            validText="4자리 숫자를 입력해주세요"
-            initialValue=""
-            className={styles.input}
-          ></Input>
-          <Button size="lg">히어로로 변신하러 가기</Button>
-        </form>
-      </div>
-    </>
-  );
+export default function HeroRegister() {
+  return <Register></Register>;
 }
