3497a774e390756a9cbc3c1c22adaa327b876fbe
CHAERYEON-LEE <sogogijjang@naver.com>
2022-12-16 00:52:27 +0900

    feat: Create Complete component
    히어로 완성 페이지 구현
    - 히어로 정보 받아서 뿌려주기

---
+++ src/components/pages/Register/Complete/Complete.tsx
@@ -0,0 +1,27 @@
+import { HeroCard, Link } from '@/components/common';
+import styles from './Complete.module.css';
+
+export const Complete = (props: { heroInfo: any }) => {
+  const { heroInfo } = props;
+
+  return (
+    <div className={`${styles.container}`}>
+      <HeroCard
+        missionCount="0"
+        heroInfo={heroInfo}
+        className={styles.cardContainer}
+      ></HeroCard>
+      <p className={styles.subTitle}>
+        {heroInfo.name}히어로가 된 걸 축하해! 멋진 임수를 완수하길 바랄게!
+      </p>
+      <div className={styles.linkContainer}>
+        <Link href="/register" size="sm">
+          임무 수행하러 가기
+        </Link>
+        <Link href="/register" size="sm">
+          추가 히어로 등록
+        </Link>
+      </div>
+    </div>
+  );
+};
