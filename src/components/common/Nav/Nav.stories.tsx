4a21a6eba10be96ac511efbcad6cddece6bd6ec7
who0803 <10rlatkdtn@naver.com>
2022-12-19 14:46:58 +0900

    feat: Create Nav component and implement signout
    - Nav component 구현
    	- 링크 컴포넌트 2개 ('/missions', '/herolist')
    	- 기능버튼(임무 등록 or 히어로 등록)
    	- 로그아웃 (추후 설정 버튼으로 수정 예정)
    	- signout 기능 구현
    		- nextAuth를 사용하여 쿠키 삭제

---
+++ src/components/common/Nav/Nav.stories.tsx
@@ -0,0 +1,37 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Nav, NavProps } from './Nav';
+// import styles from './Link.module.css';
+
+export default {
+  title: 'Components/Nav',
+  component: Nav,
+  parameters: {
+    docs: {
+      description: {
+        component: `
+          사이트 내부에서 이동하는 Nav 컴포넌트입니다
+        `,
+      },
+    },
+  },
+} as ComponentMeta<typeof Nav>;
+
+const Template: ComponentStory<typeof Nav> = (args: NavProps) => (
+  <Nav {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  buttonName: '기본 버튼',
+};
+
+export const HeroListSelectedNav = Template.bind({});
+HeroListSelectedNav.args = {
+  buttonName: '히어로 등록',
+  currentPage: '/herolist',
+};
+export const MissionListSelectedNav = Template.bind({});
+MissionListSelectedNav.args = {
+  buttonName: '임무 등록',
+  currentPage: '/missions',
+};
