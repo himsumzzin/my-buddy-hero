a7df3cf16bd47cf2b07b3698123e7dcd7660c727
CHAERYEON-LEE <sogogijjang@naver.com>
2022-12-20 22:14:29 +0900

    fix: Amend Camera Error handling
    카메라 컴포넌트 에러핸들링 추가
    - 다시찍기를 누른 후 또다시 중복으로 눌리지 않게 하기 위해 디바운스 처리
    - 얼굴인식 안됐을 때 400 에러 받았을 시 모달을 띄워 다시 사진을 찍을 수 있도록 처리
    - dialog 컴포넌트 이용해서 다시 찍으라는 안내메세지 담은   모달 추가

--- src/components/common/HeroCard/HeroCard.tsx
+++ src/components/common/HeroCard/HeroCard.tsx
@@ -14,13 +14,15 @@ export const HeroCard = ({ hero, className }: HeroCardProps) => {
         <p className={`${styles.completeNumber}`}>
           임무완료 : {completeNumber}회
         </p>
-        <Image
-          className={`${styles.cartoonize}`}
-          src={profileImage}
-          alt={`${name} 히어로`}
-          width={190}
-          height={350}
-        ></Image>
+        <div className={styles.cartoonizeContainer}>
+          <Image
+            className={`${styles.cartoonize}`}
+            src={profileImage}
+            alt={`${name} 히어로`}
+            width={190}
+            height={350}
+          ></Image>
+        </div>
         <p className={`${styles.title}`}>{title}</p>
         <p className={`${styles.name}`}>{name}</p>
         <p className={`${styles.description}`}>{description}</p>
