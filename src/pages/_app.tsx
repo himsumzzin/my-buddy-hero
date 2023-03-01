fd537555639eede1ed5c6dd1b3facf967c45defd
who0803 <10rlatkdtn@naver.com>
2023-03-01 17:09:36 +0900

    feat: Implement client session handling instead of server side session handling
    serverSideProps를 사용하여 server side에서 session check를 하는 대신에
    client 단에서 session check를 한다.
    
    client session 확인하기 위해서는 각 페이지마다 Component.auth를
    설정해주어야 한다.
    auth 설명
    - entrance (필수) : 페이지 입장 조건
    	- notLoggedIn : session이 없을 경우에만 입장 가능한 페이지임을
    	  표시
    	- loggedIn : session이 있을 경우에만 입장 가능한 페이지임을 표시
    	- sessionLoggedIn : session 스토리지에 'admin'이 존재할 경우에만
    	  입장 가능한 페이지임을 표시 (관리자 로그인 용)
    	- notSessionLoggedIn : session 스토리지에 'admin'이 존재하지
    	  않을 경우에만 입장 가능한 페이지임을 표시 (관리자 로그인 용)
    	- notEntered : 입장 불가 페이지임을 표시
    - redirection (필수) : 해당 페이지에 입장하지 못할 경우에 redirect될
      페이지
    - seconRedirection (선택) : 두 번째 redirect 주소
    
    타입 설정
    node_modules/next/dist/shared/lib/utils.d.ts 하단에 아래 코드 추가
    
    ```
    export interface AuthEnabledComponentConfig {
        auth: {
            entrance: 'loggedIn' | 'notLoggedIn' | 'sessionLoggedIn' | 'notSessionLoggedIn' | 'notEntered',
            redirection: string,
    	secondRedirection?: string
        };
    }
    
    // eslint-disable-next-line @typescript-eslint/no-explicit-any
    export type ComponentWithAuth<PropsType = any> = React.FC<PropsType> &
    AuthEnabledComponentConfig;
    ```

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,34 @@
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import Head from 'next/head';
+import { SessionProvider, useSession } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { ErrorBoundary } from '@/components';
+import Head from 'next/head';
+import { NextComponentType, NextPageContext } from 'next';
+import { AuthEnabledComponentConfig } from 'next/dist/shared/lib/utils';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import '../../public/fonts/style.css';
 import '../styles/globals.css';
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+type AppAuthProps = AppProps & {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Component: NextComponentType<NextPageContext, any, {}> &
+    Partial<AuthEnabledComponentConfig>;
+};
+interface IAuth {
+  entrance:
+    | 'notLoggedIn'
+    | 'loggedIn'
+    | 'sessionLoggedIn'
+    | 'notSessionLoggedIn'
+    | 'notEntered';
+  redirection: string;
+  secondRedirection?: string;
+}
+export default function App({ Component, pageProps }: AppAuthProps) {
   return (
     <ErrorBoundary>
       <RecoilRoot>
@@ -22,11 +41,49 @@ export default function App({ Component, pageProps }: AppProps) {
                   content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
                 />
               </Head>
-              <Component {...pageProps} />
+              <Auth auth={Component.auth as IAuth}>
+                <Component {...pageProps} />
+              </Auth>
             </>
           </SessionProvider>
         </QueryClientProvider>
       </RecoilRoot>
     </ErrorBoundary>
   );
+}
+
+export type AuthProps = {
+  auth: IAuth;
+  children: ReactNode;
+};
+function Auth({ auth, children }: AuthProps): any {
+  const router = useRouter();
+  const { status } = useSession();
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+  const adminId = sessionStorage.getItem('admin');
+  switch (auth.entrance) {
+    case 'notEntered':
+      if (status === 'authenticated')
+        router.replace(auth.redirection as string);
+      else router.replace(auth.secondRedirection as string);
+      break;
+    case 'loggedIn':
+      if (status === 'authenticated') return children;
+      else router.replace(auth.redirection as string);
+      break;
+    case 'notLoggedIn':
+      if (status === 'unauthenticated') return children;
+      else router.replace(auth.redirection as string);
+      break;
+    case 'sessionLoggedIn':
+      if (adminId) return children;
+      else router.replace(auth.redirection as string);
+      break;
+    case 'notSessionLoggedIn':
+      if (!adminId) return children;
+      else router.replace(auth.redirection as string);
+      break;
+  }
 }
